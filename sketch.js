ced5a7b8cd22e010cc620e757e50fcb35a6b963c
anikajkrishna <86082632+anikajkrishna@users.noreply.github.com>
2022-01-03 19:50:08 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,26 +1,39 @@
 var path;
-
+var runner
 
 function preload(){
-path = loadImage("path.png");
-path=createSprite (200,200);
-path.velocityY=4;
-path.scale=1.2;
-
-if (path.y > 400) {
-  path.y=height/2;
-}
-
-runner = loadImage("Runner-1.png");
-runner=createSprite()
+runnerImg = loadImage("Runner-1.png");
+pathImg = loadImage("path.png");
 }
 
 function setup(){
   createCanvas(400,400);
+
+  leftBoundary = createSprite(10,200,10,400);
+  rightBoundary = createSprite (390,200,10,400);
+  leftBoundary.visible=false;
+  rightBoundary.visible=false;
   //create sprites here
+  path=createSprite (200,200);
+  path.addImage(pathImg);
+  path.velocityY=4;
+  path.scale=1.2;
+  runner=createSprite(200,360);
+  runner.addImage(runnerImg);
+  runner.scale=0.08;
+  
 }
 
 function draw() {
   background(0);
+  if (path.y > 400) {
+    path.y=height/2;
+  }
+  runner.x=World.mouseX;
+  edges=createEdgeSprites();
 
+  runner.collide (edges[3]);
+  runner.collide (leftBoundary);
+  runner.collide (rightBoundary);
+  drawSprites();
 }
